0b672fd62d3f8964272fd9bc885f257563df9781
XEON <evandro535@gmail.com>
2024-12-20 18:17:37 -0300

    Adicionar arquivos de projeto.

---
+++ EscolaProva/Program.cs
@@ -0,0 +1,37 @@
+﻿using EscolaProva;
+using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                Aluno aluno = new Aluno();
+
+                Console.Write("Nome do Aluno: ");
+                aluno.Nome = Console.ReadLine();
+                Console.WriteLine("Digite tres notas ");
+                aluno.Nota1 = double.Parse(Console.ReadLine(), CI);
+                aluno.Nota2 = double.Parse(Console.ReadLine(), CI);
+                aluno.Nota3 = double.Parse(Console.ReadLine(), CI);
+
+                Console.WriteLine("NOTA FINAL = " + aluno.NotaFinal().ToString("F2", CI));
+
+                if (aluno.Aprovado())
+                {
+                    Console.WriteLine("APROVADO");
+                }
+                else
+                {
+                    Console.WriteLine("REPROVADO");
+                    Console.WriteLine("FALTARAM " + aluno.NotaRestante().ToString("F2", CI) + " PONTOS");
+                }
+            }
+        }
+    }
+}
